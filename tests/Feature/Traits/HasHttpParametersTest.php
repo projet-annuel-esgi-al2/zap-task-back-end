4a434c2f4f32f59d78b5e7378c6fbe1dac1592d8
MarcMalha <marc.malha@beanstock.com>
2025-06-27 01:52:42 +0200

    Tests(fix): remove WorkflowAction parameters checking

--- tests/Feature/Traits/HasHttpParametersTest.php
+++ tests/Feature/Traits/HasHttpParametersTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Traits;
 
 use App\Models\ServiceAction;
-use App\Models\WorkflowAction;
 use Tests\TestCase;
 
 class HasHttpParametersTest extends TestCase
@@ -13,9 +12,6 @@ class HasHttpParametersTest extends TestCase
         /** @var \App\Models\ServiceAction $serviceAction */
         $serviceAction = ServiceAction::factory()
             ->createOne(['body_parameters' => self::oneHiddenOneNotHiddenParameters()]);
-        /** @var \App\Models\WorkflowAction $serviceAction */
-        $workflowAction = WorkflowAction::factory()
-            ->createOne(['body_parameters' => self::oneHiddenOneNotHiddenParameters()]);
 
         $this->assertEqualsCanonicalizing(
             expected: [
@@ -25,20 +21,9 @@ class HasHttpParametersTest extends TestCase
                 ],
             ],
             actual : $serviceAction->body_parameters_for_api,
-        );
-
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-            ],
-            actual : $workflowAction->body_parameters_for_api,
         );
 
         $serviceAction->updateQuietly(['body_parameters' => self::bothNotHiddenParameters()]);
-        $workflowAction->updateQuietly(['body_parameters' => self::bothNotHiddenParameters()]);
         $this->assertEqualsCanonicalizing(
             expected: [
                 [
@@ -52,24 +37,9 @@ class HasHttpParametersTest extends TestCase
             ],
             actual : $serviceAction->body_parameters_for_api,
         );
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-                [
-                    'parameter_name' => 'User Password',
-                    'parameter_type' => 'password',
-                ],
-            ],
-            actual : $workflowAction->body_parameters_for_api,
-        );
 
         $serviceAction->updateQuietly(['body_parameters' => self::bothHiddenParameters()]);
-        $workflowAction->updateQuietly(['body_parameters' => self::bothHiddenParameters()]);
         $this->assertEmpty($serviceAction->body_parameters_for_api);
-        $this->assertEmpty($workflowAction->body_parameters_for_api);
     }
 
     public function test_can_fetch_correct_query_parameters(): void
@@ -77,9 +47,6 @@ class HasHttpParametersTest extends TestCase
         /** @var \App\Models\ServiceAction $serviceAction */
         $serviceAction = ServiceAction::factory()
             ->createOne(['query_parameters' => self::oneHiddenOneNotHiddenParameters()]);
-        /** @var \App\Models\WorkflowAction $serviceAction */
-        $workflowAction = WorkflowAction::factory()
-            ->createOne(['query_parameters' => self::oneHiddenOneNotHiddenParameters()]);
 
         $this->assertEqualsCanonicalizing(
             expected: [
@@ -89,20 +56,9 @@ class HasHttpParametersTest extends TestCase
                 ],
             ],
             actual : $serviceAction->query_parameters_for_api,
-        );
-
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-            ],
-            actual : $workflowAction->query_parameters_for_api,
         );
 
         $serviceAction->updateQuietly(['query_parameters' => self::bothNotHiddenParameters()]);
-        $workflowAction->updateQuietly(['query_parameters' => self::bothNotHiddenParameters()]);
         $this->assertEqualsCanonicalizing(
             expected: [
                 [
@@ -116,24 +72,9 @@ class HasHttpParametersTest extends TestCase
             ],
             actual : $serviceAction->query_parameters_for_api,
         );
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-                [
-                    'parameter_name' => 'User Password',
-                    'parameter_type' => 'password',
-                ],
-            ],
-            actual : $workflowAction->query_parameters_for_api,
-        );
 
         $serviceAction->updateQuietly(['query_parameters' => self::bothHiddenParameters()]);
-        $workflowAction->updateQuietly(['query_parameters' => self::bothHiddenParameters()]);
         $this->assertEmpty($serviceAction->query_parameters_for_api);
-        $this->assertEmpty($workflowAction->query_parameters_for_api);
     }
 
     public function test_can_fetch_correct_url_parameters(): void
@@ -141,9 +82,6 @@ class HasHttpParametersTest extends TestCase
         /** @var \App\Models\ServiceAction $serviceAction */
         $serviceAction = ServiceAction::factory()
             ->createOne(['url_parameters' => self::oneHiddenOneNotHiddenParameters()]);
-        /** @var \App\Models\WorkflowAction $serviceAction */
-        $workflowAction = WorkflowAction::factory()
-            ->createOne(['url_parameters' => self::oneHiddenOneNotHiddenParameters()]);
 
         $this->assertEqualsCanonicalizing(
             expected: [
@@ -153,20 +91,9 @@ class HasHttpParametersTest extends TestCase
                 ],
             ],
             actual : $serviceAction->url_parameters_for_api,
-        );
-
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-            ],
-            actual : $workflowAction->url_parameters_for_api,
         );
 
         $serviceAction->updateQuietly(['url_parameters' => self::bothNotHiddenParameters()]);
-        $workflowAction->updateQuietly(['url_parameters' => self::bothNotHiddenParameters()]);
         $this->assertEqualsCanonicalizing(
             expected: [
                 [
@@ -180,24 +107,9 @@ class HasHttpParametersTest extends TestCase
             ],
             actual : $serviceAction->url_parameters_for_api,
         );
-        $this->assertEqualsCanonicalizing(
-            expected: [
-                [
-                    'parameter_name' => 'User Email',
-                    'parameter_type' => 'email',
-                ],
-                [
-                    'parameter_name' => 'User Password',
-                    'parameter_type' => 'password',
-                ],
-            ],
-            actual : $workflowAction->url_parameters_for_api,
-        );
 
         $serviceAction->updateQuietly(['url_parameters' => self::bothHiddenParameters()]);
-        $workflowAction->updateQuietly(['url_parameters' => self::bothHiddenParameters()]);
         $this->assertEmpty($serviceAction->url_parameters_for_api);
-        $this->assertEmpty($workflowAction->url_parameters_for_api);
     }
 
     public static function oneHiddenOneNotHiddenParameters(): array
