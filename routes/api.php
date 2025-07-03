c985f4fc2366a0a150a0c45eb909a4f4abc38c35
MarcMalha <marc.malha@beanstock.com>
2025-07-03 21:36:44 +0200

    Refactor: move service controller methods to controller class instead of defining them directly on routes file

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,11 @@
 <?php
 
-use App\Enums\Service\Identifier;
 use App\Http\Controllers\Auth\AuthenticateUserController;
 use App\Http\Controllers\Auth\RegisterUserController;
+use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\ServiceOAuthController;
 use App\Http\Controllers\WorkflowController;
 use App\Http\Middleware\VerifyPersonalAccessToken;
-use App\Http\Resources\Api\ServiceResource;
-use App\Models\Service;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', RegisterUserController::class);
@@ -21,30 +19,13 @@ Route::middleware([VerifyPersonalAccessToken::class])->group(function () {
     Route::get('/subscriptions/{serviceIdentifier}', [ServiceOAuthController::class, 'get']);
 
     Route::prefix('{serviceIdentifier}/')->group(function () {
-        /** ServiceOAuth routes */
         Route::get('redirect', [ServiceOAuthController::class, 'redirect'])
             ->name('service-oauth-redirect');
 
-        /**
-         * @group Services and Actions
-         * Fetch actions for a specified Service
-         * */
-        Route::get('actions', function (Identifier $serviceIdentifier) {
-            return Service::with('actions')
-                ->where('identifier', $serviceIdentifier)
-                ->first()
-                ->toResource(ServiceResource::class);
-        });
+        Route::get('actions', [ServiceController::class, 'get']);
     });
 
-    /**
-     * @group Services and Actions
-     *
-     * Fetch all available services
-     */
-    Route::get('services', function () {
-        return Service::all()->toResourceCollection(ServiceResource::class);
-    });
+    Route::get('services', [ServiceController::class, 'index']);
 
     Route::prefix('/workflows')->group(function () {
         Route::get('/', [WorkflowController::class, 'index']);
