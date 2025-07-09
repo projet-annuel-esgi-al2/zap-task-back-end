9d29dfdbdb3e2573b503480189e8d8c1776659a2
MarcMalha <marc.malha@beanstock.com>
2025-07-09 17:15:20 +0200

    Feat: add services data to get workflow endpoint

--- app/Models/Traits/HasHttpParameters.php
+++ app/Models/Traits/HasHttpParameters.php
@@ -3,17 +3,21 @@
 namespace App\Models\Traits;
 
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Support\Arr;
 
 trait HasHttpParameters
 {
     private static function prepareParametersForApi(array $parameters): array
     {
         return collect($parameters)
-            ->filter(fn (array $parameter) => $parameter['hidden'] === false)
+            ->filter(fn (array $parameter) => is_null(Arr::get($parameter, 'hidden')) || Arr::get($parameter, 'hidden') === false)
             ->map(function (array $parameter) {
-                unset($parameter['hidden']);
 
-                return $parameter;
+                return Arr::only($parameter, [
+                    'parameter_name',
+                    'parameter_key',
+                    'parameter_type',
+                ]);
             })
             ->toArray();
     }
@@ -39,10 +43,22 @@ trait HasHttpParameters
         );
     }
 
+    protected function headersForApi(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => self::prepareParametersForApi($this->headers), // @phpstan-ignore-line
+        );
+    }
+
     protected function parametersForApi(): Attribute
     {
         return Attribute::make(
-            get : fn () => array_merge($this->body_parameters_for_api, $this->query_parameters_for_api, $this->url_parameters_for_api), // @phpstan-ignore-line
+            get : fn () => array_merge(
+                $this->body_parameters_for_api,
+                $this->query_parameters_for_api,
+                $this->url_parameters_for_api,
+                $this->headers_for_api,
+            ), // @phpstan-ignore-line
         );
     }
 }
