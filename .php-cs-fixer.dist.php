7d28cb07dda1d9bcf04a470d273bb6d089833cd3
MarcMalha <marc.malha@beanstock.com>
2025-07-13 15:26:11 +0200

    Chore: add author and version to all files using php-cs-fixer Refactor: delete unused files

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$header = <<<EOF
+Author: Marc Malha
+Version: 1.0
+EOF;
+
+return (new PhpCsFixer\Config)
+    ->setRules([
+        '@PSR12' => true,
+        'header_comment' => ['header' => $header, 'location' => 'after_open'],
+    ])
+    ->setFinder(PhpCsFixer\Finder::create()->in([
+        __DIR__ . '/app',
+        __DIR__ . '/config',
+        __DIR__ . '/database',
+        __DIR__ . '/routes',
+    ]));
